23929ec29da368e47a4b12d6f0db2f601a207faa
Abi <reidabi6@gmail.com>
2026-04-23 10:07:50 +0100

    comments

--- Assets/Scripts/BombSpawn.cs
+++ Assets/Scripts/BombSpawn.cs
@@ -7,7 +7,8 @@ public class BombSpawn : MonoBehaviour
     public float spawnDelay = 1.0f;
     public GameObject BombPrefab;
 
-    public GameObject[] bombObjects; 
+    public GameObject[] bombObjects; //list for the objects 
+
     void Start()
     {
         StartCoroutine(SpawnPrefabs());
@@ -17,9 +18,9 @@ public class BombSpawn : MonoBehaviour
     {
         while (true)
         {
-            int randomIndex = Random.Range(0, bombObjects.Length);
+            int randomIndex = Random.Range(0, bombObjects.Length); //picks a random object of the list 
             Vector3 randomSpawnPoint = new Vector3(Random.Range(15f, -15f), Random.Range(9f, 10f), Random.Range(-10f, 10f));
-            Instantiate(bombObjects[randomIndex], randomSpawnPoint, Quaternion.identity);
+            Instantiate(bombObjects[randomIndex], randomSpawnPoint, Quaternion.identity); //instantiates the random object
             yield return new WaitForSeconds(spawnDelay);
         }
     }
