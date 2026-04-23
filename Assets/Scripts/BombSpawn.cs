0800db7781eed3cb28656980a206e181f9d8edba
Abi <reidabi6@gmail.com>
2026-04-23 10:02:36 +0100

    hhfhfh

--- Assets/Scripts/BombSpawn.cs
+++ Assets/Scripts/BombSpawn.cs
@@ -7,6 +7,7 @@ public class BombSpawn : MonoBehaviour
     public float spawnDelay = 1.0f;
     public GameObject BombPrefab;
 
+    public GameObject[] bombObjects; 
     void Start()
     {
         StartCoroutine(SpawnPrefabs());
@@ -16,8 +17,9 @@ public class BombSpawn : MonoBehaviour
     {
         while (true)
         {
+            int randomIndex = Random.Range(0, bombObjects.Length);
             Vector3 randomSpawnPoint = new Vector3(Random.Range(15f, -15f), Random.Range(9f, 10f), Random.Range(-10f, 10f));
-            Instantiate(BombPrefab, randomSpawnPoint, Quaternion.identity);
+            Instantiate(bombObjects[randomIndex], randomSpawnPoint, Quaternion.identity);
             yield return new WaitForSeconds(spawnDelay);
         }
     }
